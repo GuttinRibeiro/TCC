0a0aa0083cb29220cee226b87ed17410e552e40b
GuttinRibeiro <augustoribeiro97@gmail.com>
2020-06-25 00:37:26 -0300

    Adicionando soccerview

--- controller/src/map.cpp
+++ controller/src/map.cpp
@@ -3,6 +3,19 @@
 #include <string>
 #include <iostream>
 #include "map/map_node.hpp"
+#include "utils/fields/field_ssl2019.hpp"
+#include <QApplication>
+#include <thread>
+
+void runROS(int argc, char **argv, std::string team, int id, Field *field) {
+  //ROS 2
+  rclcpp::init(argc, argv);
+  rclcpp::executors::MultiThreadedExecutor executor;
+  auto map_node = std::make_shared<Map_Node>(team, id, field);
+  executor.add_node(map_node);
+  std:: cout << "Number of threads: "<< executor.get_number_of_threads() << "\n";
+  executor.spin();
+}
 
 int main(int argc, char **argv) {
   // Command line argument:
@@ -15,11 +28,24 @@ int main(int argc, char **argv) {
   std::string team = argv[1];
   int id = atoi(argv[2]);
 
-  //ROS 2
+  Field_SSL2019 field;
+
+  QApplication app(argc, argv);
+/*  //ROS 2
   rclcpp::init(argc, argv);
   rclcpp::executors::MultiThreadedExecutor executor;
-  auto map_node = std::make_shared<Map_Node>(team, id, 60);
+  auto map_node = std::make_shared<Map_Node>(team, id, &field);
   executor.add_node(map_node);
+  std:: cout << "Number of threads: "<< executor.get_number_of_threads() << "\n";
+  executor.spin();*/
+
+  std::thread rosThread(runROS, argc, argv, team, id, &field);
+
+  // Block main thread
+  int retn = app.exec();
+  rosThread.join();
+
+  // Stop node
   rclcpp::shutdown();
-  return 0;
+  return retn;
 }
--- controller/src/map/map_node.hpp
+++ controller/src/map/map_node.hpp
@@ -12,25 +12,38 @@
 #include "ctr_msgs/msg/visionpkg.hpp"
 #include "ctr_msgs/srv/inforequest.hpp"
 #include "ctr_msgs/srv/positionrequest.hpp"
+
+// My libs
 #include "worldmap.hpp"
+#include "gui/soccerview.hh"
+#include "../utils/field.hpp"
 
 class Map_Node : public rclcpp::Node {
 private:
   qint8 _id;
   std::string _team;
   WorldMap *_wm;
+  Field *_field;
   timespec _start, _stop;
   double _time_now;
 
+  // Graphical client
+  GLSoccerView *_view;
+
+  // Callback groups to separate work into queues
   rclcpp::CallbackGroup::SharedPtr _callback_group_vision;
   rclcpp::CallbackGroup::SharedPtr _callback_worldmap_update;
   rclcpp::CallbackGroup::SharedPtr _callback_information_services;
+  rclcpp::CallbackGroup::SharedPtr _callback_gui;
 
+  // ROS interfaces
   rclcpp::Subscription<ctr_msgs::msg::Visionpkg>::SharedPtr _subVision;
   rclcpp::TimerBase::SharedPtr _timerUpdate;
   rclcpp::Service<ctr_msgs::srv::Inforequest>::SharedPtr _infoService;
   rclcpp::Service<ctr_msgs::srv::Positionrequest>::SharedPtr _posService;
+  rclcpp::TimerBase::SharedPtr _timerGUI;
 
+  void updateGUI();
   void visionCallback(const ctr_msgs::msg::Visionpkg::SharedPtr msg);
   void updateWorldMap();
   void getInformation(const std::shared_ptr<rmw_request_id_t> request_header,
@@ -40,7 +53,7 @@ private:
                       const std::shared_ptr<ctr_msgs::srv::Positionrequest::Request> request,
                       const std::shared_ptr<ctr_msgs::srv::Positionrequest::Response> response);
 public:
-  Map_Node(const std::string team, const int id, int frequency);
+  Map_Node(const std::string team, const int id, Field *f, int frequency = 60);
   ~Map_Node();
 };
 
